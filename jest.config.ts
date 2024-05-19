0d4d888dbb5e179f3b785c8ca6e1a8d67f17d24c
TRIPLE-ADE <adetomiwaabdul@gmail.com>
2024-05-19 12:28:40 +0100

    feat: add hero component and update Jest configuration

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,16 @@
 export default {
-    preset: 'ts-jest',
-    testEnvironment: 'jest-environment-jsdom',
-    transform: {
-        "^.+\\.tsx?$": "ts-jest" 
-    // process `*.tsx` files with `ts-jest`
-    },
-    moduleNameMapper: {
-        '^@/(.*)$': '<rootDir>/src/$1',
-        '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__ mocks __/fileMock.js',
-    },
-}
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/fileTransformer.cjs',
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "\\.(gif|ttf|eot|svg|png|webp)$":
+      "<rootDir>/src/test/__mocks__/fileMock.js",
+  },
+  testMatch: ["<rootDir>/src/**/*.test.tsx"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+};
---
+++ src/components/index.tsx
@@ -0,0 +1,5 @@
+import Header from "./Header";
+import Hero from "./Hero";
+import Layout from "./Layout";
+
+export { Header, Hero, Layout };
