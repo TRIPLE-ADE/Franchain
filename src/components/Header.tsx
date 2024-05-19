fd80ea7ffa91785360d57832e0e7e4c647ae706d
TRIPLE-ADE <adetomiwaabdul@gmail.com>
2024-05-19 10:15:04 +0100

    refactor: update Header component and button variants

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,16 @@
-import { Button } from "./ui/button"
+import { Button } from "./ui/button";
 
 const Header = () => {
   return (
     <div className="flex items-center justify-between py-16">
-        <h1 className="text-[40px] font-medium text-neutral-900">Good morning, Jonathan</h1>
-        <Button size="lg" variant="secondary" className="font-bold">Create collection</Button>
+      <h1 className="text-[40px] font-medium text-neutral-900">
+        Good morning, Jonathan
+      </h1>
+      <Button size="lg" variant="secondary" className="font-bold">
+        Create collection
+      </Button>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
